f67fda81fb1709d7c4aed44d8dfcd833a42a053d
cocoindoglobal <support@cocoindoglobal.com>
2025-08-05 21:57:17 +0700

    add products page

---
+++ pages/products.js
@@ -0,0 +1,16 @@
+export default function Products() {
+  return (
+    <div className="min-h-screen p-8 bg-white text-gray-800">
+      <h1 className="text-3xl font-bold mb-4">Our Coconut Charcoal Briquettes</h1>
+      <ul className="list-disc ml-6 space-y-2">
+        <li>Type: Hexagonal Coconut Charcoal Briquette</li>
+        <li>Moisture: &lt; 8%</li>
+        <li>Ash Content: &lt; 3%</li>
+        <li>Fixed Carbon: &gt; 75%</li>
+        <li>Packaging: PP bag 20kg</li>
+        <li>MOQ: 1x20ft Container</li>
+        <li>Capacity: 50 tons/month</li>
+      </ul>
+    </div>
+  );
+}
