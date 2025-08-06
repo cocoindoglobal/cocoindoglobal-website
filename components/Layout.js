e4eb8ac3f5f24815b7840297dd7d954cc60bf641
cocoindoglobal <support@cocoindoglobal.com>
2025-08-06 19:01:56 +0700

    fix logo path in navbar

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
       {/* Navbar */}
      <nav className="bg-gray-100 p-4 shadow">
   <div className="max-w-6xl mx-auto flex gap-6 text-sm font-medium items-center">
-    <img src="/COCO INDO GLOBAL LOGO.png" alt="Coco Indo Global Logo" className="h-10 mr-6" />
+  <img src="/COCO_INDO_GLOBAL_LOGO.png" alt="Coco Indo Global Logo" className="h-10 mr-6" />
     <Link href="/" className="hover:text-green-600">Home</Link>
     <Link href="/products" className="hover:text-green-600">Products</Link>
     <Link href="/about" className="hover:text-green-600">About</Link>
